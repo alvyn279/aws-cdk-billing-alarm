035a28ad1d51e3ab7b147917e7d6b23324734383
alvyn le <alvyn279@gmail.com>
2021-12-25 14:18:38 -0500

    feat: migrate to aws-cdk-lib 2.x

--- lib/billing-alarm.ts
+++ lib/billing-alarm.ts
@@ -1,8 +1,9 @@
-import * as cdk from '@aws-cdk/core';
-import * as cw from '@aws-cdk/aws-cloudwatch';
-import * as cwa from '@aws-cdk/aws-cloudwatch-actions';
-import * as sns from '@aws-cdk/aws-sns';
-import * as sub from '@aws-cdk/aws-sns-subscriptions';
+import { Duration } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as cw from 'aws-cdk-lib/aws-cloudwatch';
+import * as cwa from 'aws-cdk-lib/aws-cloudwatch-actions';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as sub from 'aws-cdk-lib/aws-sns-subscriptions';
 
 /**
  * Properties for a BillingAlarm
@@ -27,45 +28,58 @@ export interface BillingAlarmProps {
  * Note: The email addresses used as SNS Topic endpoint must be manually confirmed
  * once the stack is deployed.
  */
-export class BillingAlarm extends cdk.Construct {
-  constructor(scope: cdk.Construct, id: string, props: BillingAlarmProps) {
+export class BillingAlarm extends Construct {
+  constructor(scope: Construct, id: string, props: BillingAlarmProps) {
     super(scope, id);
 
     if (props.emails.length === 0) {
-      throw new Error('Cannot supply an empty array of email subscriptions');
+      throw new Error(
+        'Cannot supply an empty array of email subscriptions',
+      );
     }
 
-    const billingAlarmTopic: sns.ITopic = new sns.Topic(this, 'BillingAlarmNotificationTopic', {
-      topicName: 'BillingAlarmNotificationTopic',
-    });
+    const billingAlarmTopic: sns.ITopic = new sns.Topic(
+      this,
+      'BillingAlarmNotificationTopic',
+      {
+        topicName: 'BillingAlarmNotificationTopic',
+      },
+    );
 
     props.emails.forEach((email: string) => {
-      billingAlarmTopic.addSubscription(new sub.EmailSubscription(email, {
-        json: true,
-      }));
+      billingAlarmTopic.addSubscription(
+        new sub.EmailSubscription(email, {
+          json: true,
+        }),
+      );
     });
 
     const billingAlarmMetric: cw.Metric = new cw.Metric({
       metricName: 'EstimatedCharges',
       namespace: 'AWS/Billing',
       statistic: 'Maximum',
-      dimensions: {
+      dimensionsMap: {
         Currency: 'USD',
       },
     }).with({
       // Evaluates the metric every 9 hours. This is needed because
       // `EstimatedCharges` metrics is updated every 6 hours
       // See: https://forums.aws.amazon.com/thread.jspa?threadID=135955
-      period: cdk.Duration.hours(9),
+      period: Duration.hours(9),
     });
 
-    const billingAlarm: cw.Alarm = new cw.Alarm(this, 'BillingCloudWatchAlarm', {
-      alarmDescription: 'Upper monthly billing cost limit',
-      comparisonOperator: cw.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
-      evaluationPeriods: 1,
-      metric: billingAlarmMetric,
-      threshold: props.monthlyThreshold,
-    });
+    const billingAlarm: cw.Alarm = new cw.Alarm(
+      this,
+      'BillingCloudWatchAlarm',
+      {
+        alarmDescription: 'Upper monthly billing cost limit',
+        comparisonOperator:
+          cw.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+        evaluationPeriods: 1,
+        metric: billingAlarmMetric,
+        threshold: props.monthlyThreshold,
+      },
+    );
 
     const alarmAction: cwa.SnsAction = new cwa.SnsAction(billingAlarmTopic);
 
